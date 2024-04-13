55bae98d60e020b48419c11f1334a3f274416b22
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2024-04-13 08:33:50 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NowPlaying
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
