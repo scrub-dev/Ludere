46b077058f469e270133c5e644875a78a4672d9c
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2024-04-15 22:11:08 +0100

    * Added Fonts * Changed to flatter theme * Button -> RadioButton for Navigation

--- Core/ObservableObject.cs
+++ Core/ObservableObject.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NowPlaying.ViewModels
+namespace NowPlaying.Core
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName) 
+
+        protected void OnPropertyChanged([CallerMemberName]string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,38 +19,63 @@ namespace NowPlaying
     public partial class MainWindow : Window
     {
 
-        private Dictionary<String, UserControl> frames = [];
+        private readonly Dictionary<String, Type> frames = [];
+        private Type? _currentlyVisibleFrame;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            frames.Add("HOME", new Views.HomeView());
-            frames.Add("SPOTIFYSETTINGS", new Views.SpotifySettingsView());
-            frames.Add("SERVERSETTINGS", new Views.ServerSettingsView());
-            frames.Add("STYLESETTINGS", new Views.StyleSettingsView());
-            frames.Add("CUSTOMSTYLE", new Views.CustomStyleView());
+            frames.Add("HOME",            typeof(Views.HomeView));
+            frames.Add("SPOTIFYSETTINGS", typeof(Views.SpotifySettingsView));
+            frames.Add("SERVERSETTINGS",  typeof(Views.ServerSettingsView));
+            frames.Add("STYLESETTINGS",   typeof(Views.StyleSettingsView));
+            frames.Add("CUSTOMSTYLE",     typeof(Views.CustomStyleView));
 
-            FrameMain.Content = frames["HOME"];
+            SetFrameContent(FrameMain, typeof(Views.HomeView));
         }
 
         private void BtnHelp_Click(object sender, RoutedEventArgs e)
         {
             Util.OpenUri(Properties.Settings.Default.HelpURI);
         }
+        private Type GetFrame (string frameString)
+        {
+            return frames[frameString.Split("_")[1].ToUpper()];
+        }
+        private void SetFrameContent (Frame rootFrame, Type subframe)
+        {
+            if (_currentlyVisibleFrame == subframe.GetType()) return;
+            if (rootFrame.NavigationService.CanGoBack) rootFrame.NavigationService.RemoveBackEntry();
+
+            UserControl FrameToNavigateTo = (UserControl)Activator.CreateInstance(subframe)! ?? new Views.HomeView();
+            FrameMain.Navigate(FrameToNavigateTo);
+            _currentlyVisibleFrame = FrameToNavigateTo.GetType();
+        }
 
         private void BtnNav_Click(object sender, RoutedEventArgs e)
         {
-            if(!sender.GetType().Equals(typeof(System.Windows.Controls.Button))){
-                return;
-            }
-            string nameString = (((Button)sender).Name.Split("_")[1]).ToUpper();
+            if(!sender.GetType().Equals(typeof(Button))) return;
 
-            if (nameString is null) return;
+            Type FrameToShow = GetFrame(((Button)sender).Name);
+            SetFrameContent(FrameMain, FrameToShow);
+        }
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (!sender.GetType().Equals(typeof(Button))) return;
 
-            UserControl clickedFrame = frames[nameString];
+            Application.Current.Shutdown();
+        }
 
-            FrameMain.Navigate(clickedFrame);
+        private void BtnMinimise_Click(object sender, RoutedEventArgs e)
+        {
+            if (!sender.GetType().Equals(typeof(Button))) return;
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
         }
     }
 }
