e558cbd9a1e2dc8c0df614c063248bf236f057cc
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2024-04-25 16:05:46 +0100

    Feature Toggles using Bitflags

---
+++ Utility/FeatureHandler.cs
@@ -0,0 +1,38 @@
+﻿using NowPlaying.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowPlaying.Utility
+{
+    internal class FeatureHandler
+    {
+        [Flags]
+        public enum Feature
+        {
+            SONG_NAME = 1 << 1,
+            ARTIST_NAME = 1 << 2,
+            ALBUM_NAME = 1 << 3,
+            ARTWORK = 1 << 4,
+            SONG_PROGRESSION = 1 << 5
+        }
+        public static void ToggleFeature(Feature f, bool val)
+        {
+            if (val) EnableFeature(f);
+            else DisabledFeature(f);
+        }
+        public static void DisabledFeature(Feature f)
+        {
+            Settings.Default.EnabledFeatures &= ~(int)f;
+            Settings.Default.Save();
+        }
+        public static void EnableFeature(Feature f)
+        {
+            Settings.Default.EnabledFeatures |= (int)f;
+            Settings.Default.Save();
+        }
+        public static bool IsFeatureEnabled(Feature f) => (Settings.Default.EnabledFeatures & (int)f) != 0;
+    }
+}
--- ViewModels/StyleSettingsViewModel.cs
+++ ViewModels/StyleSettingsViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using NowPlaying.Core;
+using NowPlaying.Properties;
+using NowPlaying.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,14 @@ using System.Threading.Tasks;
 
 namespace NowPlaying.ViewModels
 {
-    internal class StyleSettingsViewModel
+    internal class StyleSettingsViewModel : ObservableObject
     {
+
+        public bool SongProgressionChecked { get => FeatureHandler.IsFeatureEnabled(FeatureHandler.Feature.SONG_PROGRESSION); set { FeatureHandler.ToggleFeature(FeatureHandler.Feature.SONG_PROGRESSION, value) ; OnPropertyChanged(); } }
+        public bool ArtworkChecked { get => FeatureHandler.IsFeatureEnabled(FeatureHandler.Feature.ARTWORK); set { FeatureHandler.ToggleFeature(FeatureHandler.Feature.ARTWORK, value); OnPropertyChanged(); } }
+        public bool ArtistChecked { get => FeatureHandler.IsFeatureEnabled(FeatureHandler.Feature.ARTIST_NAME); set { FeatureHandler.ToggleFeature(FeatureHandler.Feature.ARTIST_NAME, value); OnPropertyChanged(); } }
+        public bool AlbumChecked { get => FeatureHandler.IsFeatureEnabled(FeatureHandler.Feature.ALBUM_NAME); set { FeatureHandler.ToggleFeature(FeatureHandler.Feature.ALBUM_NAME, value); OnPropertyChanged(); } }
+        public bool SongNameChecked { get => FeatureHandler.IsFeatureEnabled(FeatureHandler.Feature.SONG_NAME); set { FeatureHandler.ToggleFeature(FeatureHandler.Feature.SONG_NAME, value); OnPropertyChanged(); } }
+
     }
 }
